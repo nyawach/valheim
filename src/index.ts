f658112807f53b6736fae5eb1345cb4ff6d7bb07
himeno-yusuke <himeno-yusuke@kayac.com>
2021-03-30 23:51:40 +0900

    webhook url 登録できるまで

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,43 @@
-import * as DiscordJS from 'discord.js'
-
-import dotenv from 'dotenv'
-import { Request } from 'express'
+/**
+ * @ref https://github.com/discord/discord-interactions-js/blob/main/examples/gcloud_function.js
+ */
+import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions'
 import { Response } from 'express'
-
+import dotenv from 'dotenv'
 dotenv.config()
 
-export const main = (req: Request, res: Response) => {
-  console.log(req, process.env)
-  const client = new DiscordJS.Client()
-  
-  client.on('ready', async () => {
-    console.log('the bot is ready')
-  })
+const CLIENT_PUBLIC_KEY = process.env.CLIENT_PUBLIC_KEY
 
-  res.status(200)
-  res.send({message: 'Hello'})
+export const main = async (req: any, res: Response) => {
+  if(!CLIENT_PUBLIC_KEY) {
+    return res.status(401).send('invalid request signature')
+  }
+
+  const sig = req.get('x-signature-ed25519')
+  const time = req.get('x-signature-timestamp')
+
+  if(!sig || !time) {
+    return res.status(401).send('invalid request signature')
+  }
+
+  const isValid = await verifyKey(req.rawBody, sig, time, CLIENT_PUBLIC_KEY)
+
+  if (!isValid) {
+    return res.status(401).send('invalid request signature')
+  }
+
+  const interaction = req.body
+  if(interaction && interaction.type === InteractionType.APPLICATION_COMMAND) {
+    res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: `${interaction.data.name}コマンド用アプリです`
+      },
+    })
+  } else {
+    res.send({
+      type: InteractionResponseType.PONG,
+    })
+  }
+  return res.status(401).send('invalid request signature')
 }
