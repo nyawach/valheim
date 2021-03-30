f983606eabac7eec5bf73e3c295a2740962d64da
himeno-yusuke <himeno-yusuke@kayac.com>
2021-03-30 22:27:13 +0900

    Cloud Functionへのデプロイと確認

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import * as DiscordJS from 'discord.js'
 
 import dotenv from 'dotenv'
+import { Request } from 'express'
+import { Response } from 'express'
 
 dotenv.config()
 
-const client = new DiscordJS.Client()
+export const main = (req: Request, res: Response) => {
+  console.log(req, process.env)
+  const client = new DiscordJS.Client()
+  
+  client.on('ready', async () => {
+    console.log('the bot is ready')
+  })
 
-client.on('ready', async () => {
-  console.log('the bot is ready')
-})
+  res.status(200)
+  res.send({message: 'Hello'})
+}
